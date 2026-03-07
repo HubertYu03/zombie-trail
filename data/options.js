e113aa90b4487af0420dff742fe6fc68902c060e
Hubert Yu <hubyu9234@gmail.com>
2026-03-07 09:10:52 -0800

    feat(options): add options

---
+++ data/options.js
@@ -0,0 +1,20 @@
+export const titleOptions = {
+    title: null,
+    options: [
+        {
+            key: 1,
+            label: "Drive the trail",
+            destination: "originSelect"
+        },
+        {
+            key: 2,
+            label: "Read the lore",
+            destination: "lore"
+        },
+        {
+            key: 3,
+            label: "Learn how the game works",
+            destination: "tutorial"
+        }
+    ]
+}
--- src/title.js
+++ src/title.js
@@ -1,6 +1,15 @@
+import { titleOptions } from "../data/options"
+
 const choice = document.getElementById("choice")
 const title = document.getElementById("title")
 const question = document.getElementById("question")
+const options = document.getElementById("options")
+
+function startGame() {
+    for (const op of titleOptions.options) {
+
+    }
+}
 
 choice.addEventListener("keydown", (event) => {
     let input = choice.value
