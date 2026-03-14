0e5c2e2524dc3aa1a399cf072d2ce898389eb4ad
Hubert Yu <hubyu9234@gmail.com>
2026-03-14 09:04:15 -0700

    feat(options): navigation through pages logic complete

--- src/title.js
+++ src/title.js
@@ -1,21 +1,45 @@
-import { titleOptions } from "../data/options"
+import { optionData } from "../data/optionData"
 
 const choice = document.getElementById("choice")
 const title = document.getElementById("title")
 const question = document.getElementById("question")
+const youMay = document.getElementById("you-may")
 const options = document.getElementById("options")
 
-function startGame() {
-    for (const op of titleOptions.options) {
+let currentData;
 
+function loadGame() {
+    currentData = optionData["title"]
+
+    for (const op of currentData.options) {
+        const li = document.createElement("li")
+        li.textContent = op.prompt
+        options.appendChild(li)
     }
+}
+
+function loadUI() {
+    clearUI()
+
+    question.textContent = currentData.text
+}
+
+function clearUI() {
+    youMay.textContent = ""
+    options.innerHTML = ""
 }
 
 choice.addEventListener("keydown", (event) => {
     let input = choice.value
 
     if (input && event.key == "Enter") {
+        console.log("Test");
 
+        const destination = currentData.options[input - 1].dest
+        currentData = optionData[destination]
+
+        loadUI()
+        choice.value = null
     }
 })
 
